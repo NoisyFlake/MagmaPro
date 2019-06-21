f009f536c227f278ef88ecf70b791fe6c2535c1c
NoisyFlake <joelpoick@me.com>
2019-06-21 20:04:04 +0200

    Increase version to 1.2.0

--- preferences/MagmaPrefs.m
+++ preferences/MagmaPrefs.m
@@ -144,7 +144,7 @@
 		version.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
 		version.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:15.0f];
 		version.textColor = [UIColor colorWithRed:0.82 green:0.82 blue:0.84 alpha:1.0];
-		version.text = @"Version 1.1.1";
+		version.text = @"Version 1.2";
 		version.backgroundColor = [UIColor clearColor];
 		version.textAlignment = NSTextAlignmentCenter;
 
